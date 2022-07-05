5dfffe7da815065c5d5e7e5f989fa5aae8cacbdd
Mayur Saroj <mayursarojofficial@gmail.com>
2022-07-05 18:11:35 +0530

    :lipstick: Add user email in UserCard

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -6,8 +6,8 @@ const UserCard = (props) => {
     <div className="user-card flex gap-2 items-center mb-4 border rounded p-2 shadow">
       <img src={user.image} className="w-14 h-auto" alt="username" />
       <div className="user-details">
-        <h2 className="fullname font-semibold">{user.firstName}</h2>
-        <p className="text-sm text-slate-500">{user.lastName}</p>
+        <h2 className="fullname font-semibold">{user.firstName} {user.lastName}</h2>
+        <p className="text-sm text-slate-500">{user.email}</p>
       </div>
     </div>
   );
