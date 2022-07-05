d9417223a1cf21ad7bfd68ddfbc20f72bca0afc0
Mayur Saroj <mayursarojofficial@gmail.com>
2022-07-05 22:22:46 +0530

    :sparkles: Make list filterable

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,41 @@ import UsersList from "./UsersList";
 
 const App = () => {
   const [users, setUsers] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
 
   useEffect(() => {
     const fetchUsers = async () => {
       const resp = await fetch("https://dummyjson.com/users");
       const { users } = await resp.json();
       setUsers(users);
+      setFilteredList(users);
     };
     fetchUsers();
   }, []);
+
+  const filterList = (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    console.log(searchTerm);
+    const filteredList = users.filter((user) => {
+      const firstName = user.firstName.toLowerCase();
+      const lastName = user.lastName.toLowerCase();
+      const fullName = `${firstName} ${lastName}`;
+
+      return fullName.includes(searchTerm);
+    });
+    setFilteredList(filteredList);
+  };
+
   return (
     <div className="container mx-auto m-6">
       <input
         type="text"
         className="filterable-search border border-slate-600 rounded p-2 mb-4 w-full"
         placeholder="Search for a user"
+        onChange={filterList}
       />
-      {users.length ? <UsersList users={users} /> : <Spinner />}
+
+      {users.length ? <UsersList users={filteredList} /> : <Spinner />}
     </div>
   );
 };
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -2,11 +2,17 @@ import React from "react";
 
 const UserCard = (props) => {
   const { user } = props;
+
+  const renderFullName = () => {
+    const fullName = `${user.firstName} ${user.lastName}`;
+    return fullName;
+  };
+
   return (
     <div className="user-card flex gap-2 items-center mb-4 border rounded p-2 shadow">
       <img src={user.image} className="w-14 h-auto" alt="username" />
       <div className="user-details">
-        <h2 className="fullname font-semibold">{user.firstName} {user.lastName}</h2>
+        <h2 className="fullname font-semibold">{renderFullName()}</h2>
         <p className="text-sm text-slate-500">{user.email}</p>
       </div>
     </div>
